8f5f7f1ec4b166804ee01bba26bbf1e1b399ec26
Ali Ellabban <aliellabban2004@gmail.com>
2026-02-13 20:26:18 +0000

    dda raytracer v1 finished

--- python_testing/dda_raytracer.py
+++ python_testing/dda_raytracer.py
@@ -1,7 +1,19 @@
 import math
 
+light_pos = (8.5, 10, 8.5)
+width = 640
+height = 360
+world_size = 20
+
+colour_list = [(0, 0, 0),
+               (255, 0, 0),
+               (0, 255, 0),
+               (0, 0, 255),
+               (255, 255, 255),
+               (0, 0, 0)]
+
 def normalize(v):
-    length = math.sqrt((v[0]*v[0]) * (v[1]*v[1]) * (v[2]*v[2]))
+    length = math.sqrt((v[0]*v[0]) + (v[1]*v[1]) + (v[2]*v[2]))
 
     return (v[0]/length, v[1]/length, v[2]/length)
 
@@ -9,7 +21,7 @@ def floor(num):
     return math.floor(num)
 
 def vectAbs(v):
-    return math.sqrt((v[0]*v[0]) * (v[1]*v[1]) * (v[2]*v[2]))
+    return math.sqrt((v[0]*v[0]) + (v[1]*v[1]) + (v[2]*v[2]))
 
 def numAbs(num):
     if num < 0:
@@ -29,18 +41,38 @@ def sub(v1, v2):
 def add(v1, v2):
     return (v1[0] + v2[0], v1[1] + v2[1], v1[2]+ v2[2])
 
-def generateWorld(height, rows, cols, default_val=0):
-    # Create a 3D grid: [Height][Rows/Z][Cols/X]
-    # Initialize everything to 'default_val' (0 = Air)
-    world = [[[default_val for _ in range(cols)] for _ in range(rows)] for _ in range(height)]
+def generateWorld(width_x, height_y, depth_z, default_val=0):
+    # Create [X][Y][Z] structure
+    # Note the order of loops: 
+    #   Outer: range(width_x)
+    #   Middle: range(height_y)
+    #   Inner: range(depth_z)
+    print("generating world with size:", width_x)
+    world = [[[default_val for z in range(depth_z)] for y in range(height_y)] for x in range(width_x)]
 
-    # --- 1. Add the Floor ---
-    # We use the bottom-most layer (index 0) as the floor.
-    for z in range(rows):
-        for x in range(cols):
-            world[0][z][x] = 1  # Block ID 1 = Floor
+    # # --- Add the Floor
+    # # Now we can loop naturally using x and z
+    # for x in range(width_x):
+    #     for z in range(depth_z):
+    #         # Access strictly as [x][y][z]
+    #             world[x][0][z] = 1
+
+    check_size = 1
+
+    for x in range(width_x):
+        for z in range(depth_z):
+            # 1. Scale the coordinates down by the size
+            scaled_x = x // check_size
+            scaled_z = z // check_size
+            
+            # 2. Check if the sum is even or odd (standard checkerboard math)
+            if (scaled_x + scaled_z) % 2 == 0:
+                 world[x][0][z] = 4  # White
+            else:
+                 world[x][0][z] = 5  # Black
 
     return world
+
 
 def intersect_voxel(ray_origin, ray_direction, world):
     # Initialize the algorithm:
@@ -85,29 +117,68 @@ def intersect_voxel(ray_origin, ray_direction, world):
         normal = [0, 0, 0]
         normal[axis] = -step[axis]
 
-        # Check map_pos if we are out of bounds, return 0 on block_id and 
+        # Work out hit point
+        t = side_dist[axis] - delta_dist[axis]
+        hit_point = add(ray_origin , scalarMult(t, ray_direction))
+
+        # Check map_pos if we are out of bounds
         for i in range(3):
-            if map_pos[i] >= 16 or map_pos[i] < 0:
-                print("out of bounds")
-                return 0, (0, 0, 0)
+            if map_pos[i] >= world_size or map_pos[i] < 0:
+                return 0, (0, 0, 0), (0, 0, 0)
 
         if world[map_pos[0]][map_pos[1]][map_pos[2]] > 0:
             block_value = world[map_pos[0]][map_pos[1]][map_pos[2]]
 
-    return block_value, normal
+    return block_value, normal, hit_point
+
+def createPPM(pixel_list):
+    with open("output_dda.ppm", "w") as f:
+        f.write(f'P3\n{width} {height}\n255')
+        for color in pixel_list:
+            f.write(f'\n{color[0]} {color[1]} {color[2]}')
+
+
+def get_colour(block_id, normal_vect, hit_point):
+    # If out of bounds return black
+    if block_id == 0:
+        return (0, 0, 0)
+    
+    light_dir = normalize(sub(light_pos, hit_point))
+    intensity = max(0,dot(normal_vect, light_dir))
+    obj_color = colour_list[block_id]
+    return (int(obj_color[0] * intensity), int(obj_color[1] * intensity), int(obj_color[2] * intensity))
 
 
 def main():
-    world = generateWorld(16,16,16)
-    print(world[0][1][1])
-
     print(floor(10.1))
     print(floor(-12.3))
     print(numAbs(-123))
 
-    block_value, normal = intersect_voxel((8.5, 10.5, 8.5), (0, -1, 0), world)
-    print(block_value, normal)
+    block_value, normal, hit_point = intersect_voxel((8.5, 10.5, 8.5), (0, -1, 0), world)
+    print(block_value, normal, hit_point)
 
+def create_image():
+    print("create image")
+    fov = math.pi / 2
+    aspect_ratio = width / height
+    pixel_list = []
+    world = generateWorld(world_size, world_size, world_size)
 
-if __name__ == main():
-    main()
+    for y in range(height):
+        for x in range(width):
+            
+            # Map Screen and camera to rays and coordinates
+            Px = (2* (x + 0.5) / width  - 1) * aspect_ratio * math.tan(fov / 2)
+            Py = -(2 * (y + 0.5) / height - 1) * math.tan(fov / 2)
+
+            mapped_ray_dir = (Px, Py, 1)
+            camera_pos = (8.5, 2, 8.5)
+
+            block_value, normal, hit_point = intersect_voxel(camera_pos, normalize(mapped_ray_dir), world)
+            pixel_colour = get_colour(block_value, normal, hit_point)
+            pixel_list.append(pixel_colour)
+    
+    createPPM(pixel_list)
+
+if __name__ == "__main__":
+    create_image()
